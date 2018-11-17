5640996f10b24eb8e2761ff23ec45686faf458b1
Vivek R <123vivekr@gmail.com>
2018-11-17 10:36:31 +0530

    600:705A

---
+++ codeforces/600/705A.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, f = 1;
+    scanf("%d", &n);
+    for(int i = 0; i < n && (1 + 0 * (i != 0 && printf("that "))); ++i ) {
+        if(f == 1)
+            printf("I hate ");
+        else
+            printf("I love ");
+        f *= -1;
+    }
+    printf("it\n");
+    return 0;
+}
